dfecfab8cad58bd9132269a0f2d89db2e2664537
tajveer nijjar <taj.nijjar2002@gmail.com>
2017-04-09 21:30:16 +1200

    simple listview

--- Xamarin ListView/MainActivity.cs
+++ Xamarin ListView/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 
@@ -7,12 +8,25 @@ namespace Xamarin_ListView
     [Activity(Label = "Xamarin_ListView", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private List<string> items;
+        private ListView listView;
+
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            items = new List<string> ();
+            items.Add ("Bob");
+            items.Add ("Tom");
+            items.Add ("Jim");
+
+            listView = FindViewById<ListView> (Resource.Id.myListView);
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, items);
+
+            listView.Adapter = adapter;
+
         }
     }
 }
